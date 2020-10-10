bd46af869e0f421802f556bfc9ecbad0da45ecc8
Theelipan prabakar <theelipan@gmail.com>
2020-10-10 19:11:48 -0400

    create components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './App.css';
+import Todos from './components/Todos';
 
-function App() {
-  return (
-    <>
-      <h1>Hello</h1>
-    </>
-  );
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Study File Structure',
+        completed: false,
+      },
+      {
+        id: 2,
+        title: 'Create Component',
+        completed: false,
+      },
+      {
+        id: 3,
+        title: 'Study State',
+        completed: false,
+      },
+    ],
+  };
+
+  render() {
+    return (
+      <div className='App'>
+        <h1>Hello</h1>
+        <Todos todos={this.state.todos} />
+      </div>
+    );
+  }
 }
 
 export default App;
