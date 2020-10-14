813c2e36daea92f5ab52b3096d8398cca722ad0d
Theelipan prabakar <theelipan@gmail.com>
2020-10-13 21:36:53 -0400

    add style to todo list

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
 
+import PropTypes from 'prop-types';
+
 export class TodoItem extends Component {
+  getStyle = () => {
+    return {
+      background: '#f4f4f4',
+      padding: '.1rem',
+      margin: '.5rem auto',
+      width: '50%',
+      textAlign: 'center',
+      textDecoration: this.props.todo.completed ? 'line-through' : 'none',
+    };
+  };
   render() {
+    console.log(this.getStyle());
     return (
-      <div>
+      <div style={this.getStyle()}>
         <p>{this.props.todo.title}</p>
       </div>
     );
   }
 }
+
+TodoItem.propTypes = {
+  todos: PropTypes.object.isRequired,
+};
+
 export default TodoItem;
